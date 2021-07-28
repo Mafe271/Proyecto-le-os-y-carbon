8c60d8ac5e8bd22e503fff20f2b93ccdb1505359
Adriana Chavez <mafo2767@gmail.com>
2021-07-28 16:49:06 -0500

    parte inferior de menu

---
+++ src/Components/LinksInicio.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import restaurante from "./../utils/images/restaurante.jpg"
+
+const LinksInicio = () => {
+    return (
+        <div>
+            <div className="oficial">
+               <img src={restaurante} alt="restaurant" />
+               <a href="https://lenosycarbon.com.co/">PAGIANA OFCIAL</a>
+            </div>
+            <div className="historia">
+                <h1>NUESTRA HISTORIA</h1>
+                <P>En Leños & Carbón, junto a las Fundaciones Best Buddies y RECA, brindamos
+                   oportunidades de trabajo a personas con discapacidad intelectual, por que
+                   creemos que nuestros espacios de trabajo son más sanos cuando se enriquecen
+                   con la diversidad; y por que creemos que es responsabilidad de todos lograr
+                   una sociedad más incluyente. El aporte que ellos hacen con su trabajo brinda
+                   a nuestros invitados mejores experiencias, mas humanas y cercanas. Buscamos
+                   que al SER ELLOS MISMOS crezcan con nuestra organización y se sientan incluidos,
+                   aportando a sus familias y a la sociedad. <br/>
+                   QUEREMOS QUE EN NUESTROS RESTAURANTES HAGAMOS INDIFERENTE LA DIFERENCIA.</P>
+            </div>
+        </div>
+        
+    );
+};
+
+export default LinksInicio;
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -7,32 +7,59 @@ import entradas from '../utils/images/Entradas.png'
 import postres from '../utils/images/Postres.png'
 import sandwiches from '../utils/images/sandwiches.png'
 import coctel from '../utils/images/coctel.jpg'
+import mall from '../utils/images/mallmap.PNG'
+import airport from '../utils/images/airportmap.PNG'
+
 
 function Menu() {
   return (
-    <div className='menu'> 
+    <div className='menu'>  
 
-      <div className='menu2'>
-        <img className='img1' src={calentao}/>
-        <img className='img2' src={carnes}/>
+      <div className='menu2'>  
+        <img className='img1' src={calentao} alt="calentao"/>
+        <img className='img2' src={carnes} alt="carnes"/>
       </div>
       <div className='menu3'>
-        <img className='img3' src={carnes2}/>
+        <img className='img3' src={carnes2} alt="carnes2"/>
       </div>
       <div id='title'>
-      <a className='titleOrang' href="https://lenosycarbon.com.co/">MENÚ</a>  
+         <a className='titleOrang' href="https://lenos.imaginem.us/wp-content/uploads/menu-domicilios.pdf">MENÚ</a>  
       </div>
       <div className='menu4'>
-        <img className='img4' src={entradas}/>
-        <img className='img5' src={postres}/>
+        <img className='img4' src={entradas} alt="entradas"/>
+        <img className='img5' src={postres} alt="postres"/>
       </div>
       <div className='menu5'>
-        <img className='img6' src={sandwiches}/>
-        <img className='img7' src={coctel}/>
+        <img className='img6' src={sandwiches} alt="sandwich"/>
+        <img className='img7' src={coctel} alt="coctel"/>
       </div>
 
-    </div>
+       <div className='location'>
+            <p>Leños & Carbon <br/>
+               Centro Comercial Parque La Colina <br/>
+               Carrera 58D No. 146-51 <br/>
+               Local FC-7 <br/>
+               Tel: 6684545 Ext. 14769 <br/>
+               Cel: 3214895267 <br/>
+               Horarios de atención <br/>
+               L – D: 10:30 am – 8:30 pm <br/>
+            </p>
+            <img src={mall} alt="mall"/>
+       </div>
+       <div className='locat'>
+            <p>Leños & Carbon <br/>
+               Aeropuerto Internacional <br/>
+               El Dorado Calle 26 No. 103-09 <br/>
+               Local 105 <br/>
+               Cel: 316-834-0315 <br/>
+               Horarios de atención <br/>
+               L – D: 6:00 am – 9:00 pm <br/>
+            </p>
+            <img src={airport} alt="airport"/>
+       </div>
+
+    </div> 
   );
 }
-
-export default Menu;
+ 
+export default Menu; 
