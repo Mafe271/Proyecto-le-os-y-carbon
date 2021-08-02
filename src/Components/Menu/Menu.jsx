08c60b29c9f2e5f5346f8cd7b270d46b6c82f367
Adriana Chavez <mafo2767@gmail.com>
2021-08-02 08:39:12 -0500

    no hay cambios

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -9,6 +9,9 @@ import sandwiches from '../../utils/images/sandwiches.png'
 import coctel from '../../utils/images/coctel.jpg'
 import mall from '../../utils/images/mallmap.PNG'
 import airport from '../../utils/images/airportmap.PNG'
+import Maps from '../Menu/Maps'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 
 
@@ -58,7 +61,7 @@ function Menu() {
         </p>
         <img src={airport} alt="airport" />
       </div>
-
+        <Maps/>
     </div>
   );
 }
