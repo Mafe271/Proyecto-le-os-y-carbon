8b04ad5be0707ece04abdc09e26ff523c4b8dc6b
Adriana Chavez <mafo2767@gmail.com>
2021-08-11 14:08:36 -0500

    componente desayunos corregido

--- src/Components/OrdenMenu/Entradas.jsx
+++ src/Components/OrdenMenu/Entradas.jsx
@@ -27,25 +27,28 @@ const useStyles = makeStyles({
 });
 
 const desayunos=[
-    {producto: 'AREPA_PAISA', precio_por_unidad: '$3.500' , eliminar:'-' },
-    {producto: 'PANDEBONO', precio_por_unidad: '$4.900' , eliminar:'-' },
-    {producto: 'ALMOJÁBANA', precio_por_unidad: '$4.900' , eliminar:'-' },
-    {producto: 'PORCIÓN DE QUESO (Queso blanco) ', precio_por_unidad: '$4.900' , eliminar:'-' },
-    {producto: 'HUEVOS AL GUSTO (2 unidades)', precio_por_unidad: '' , eliminar:'-' },
-    {producto: '-Frito o revuelto', precio_por_unidad: '$5.900' , eliminar:'-' },
-    {producto: '-Con cebolla y tomate', precio_por_unidad: '$6.900' , eliminar:'-' },
-    {producto: 'COCTEL DE FRUTAS (kiwi, banano, fresa_y_melón)', precio_por_unidad: '$6.900' , eliminar:'-' },
-    {producto: 'CALDO DE COSTILLA', precio_por_unidad: '$11.900' , eliminar:'-' },
-    {producto: 'CANASTA DE ALMOJÁBANA Y PAN DE BONO', precio_por_unidad: '$13.900' , eliminar:'-' },
-    {producto: 'CALENTAO CON CHORIZO, LENTEJAS Y AREPA', precio_por_unidad: '$17.900' , eliminar:'-' },
-    {producto: 'CALENTAO CON CARNE DE RES O CARNE DE CERDO Y MADURO', precio_por_unidad: '$17.900' , eliminar:'-' },
-    {producto: 'CALENTAO PAISA (Chicharrón, frijol, maduro y aguacate)', precio_por_unidad: '$21.900' , eliminar:'-' },
-    {producto: 'BISTEC A CABALLO', precio_por_unidad: '$22.900' , eliminar:'-' }, 
-
+    {producto: 'YUCA CON HOGAO', precio_por_unidad: '$4.900' , eliminar:'-' },
+    {producto: 'PATACONES CON HOGAO 6 unidades', precio_por_unidad: '$6.900' , eliminar:'-' },
+    {producto: 'MORCILLA LEÑOS', precio_por_unidad: '$ 10.900' , eliminar:'-' },
+    {producto: '3 morcillas a la parrilla con 3 arepas  EMPANADAS', precio_por_unidad: '$13.900' , eliminar:'-' },
+    {producto: '4 unidades acompañadas Con ají de la casa y limón CHORIZOS LEÑOS', precio_por_unidad: '16.900' , eliminar:'-' },
+    {producto: '3 chorizos a la parrilla con 3 arepas PROVOLONE', precio_por_unidad: '$16.900' , eliminar:'-' },
+    {producto: 'Disponible únicamente en restaurantes de Servicio a la mesa CHICHARRONES 100g Con arepa y guacamole.', precio_por_unidad: '$17.900' , eliminar:'-' },
+   
 ];
+ 
+ 
+
+
+
+ 
+ 
+ 
+ 
+
 
 function Entradas(props) {
-     
+
       const classes = useStyle()  
       return (
           <>
