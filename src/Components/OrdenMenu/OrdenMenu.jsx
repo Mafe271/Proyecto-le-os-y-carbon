a3ea41abf8cd1a70eae34e639c50475de53e564b
Adriana Chavez <mafo2767@gmail.com>
2021-08-13 18:39:08 -0500

    botones con toggle faltan arreglos

--- src/Components/OrdenMenu/OrdenMenu.jsx
+++ src/Components/OrdenMenu/OrdenMenu.jsx
@@ -1,153 +1,159 @@
-import React, {useState} from "react"
-import { db } from '../../firebase'
-import '../OrdenMenu/ordenMenu.css'
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { db } from "../../firebase";
+import "../OrdenMenu/ordenMenu.css";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Desayunos from "../../Components/OrdenMenu/Desayunos";
-import Entradas1 from '../../Components/OrdenMenu/Entradas';
-import Res1 from "../OrdenMenu/Entradas"
-import Pescado from "../../Components/OrdenMenu/Pescado";
-import Pollo from "../../Components/OrdenMenu/Pollo";
-import Ensaladas from "../OrdenMenu/Ensaladas"
-import Sandwiches from "../../Components/OrdenMenu/Sandwiches";
-import Cocteles from "../../Components/OrdenMenu/Cocteles";
-import Postres from "../../Components/OrdenMenu/Postres";
-import Bebidas from '../OrdenMenu/Bebidas';
-
+import Entradas1 from "../../Components/OrdenMenu/Entradas";
+import Res1 from "../OrdenMenu/Entradas";
+import Pescado1 from "../../Components/OrdenMenu/Pescado";
+import Pollo1 from "../../Components/OrdenMenu/Pollo";
+import Ensaladas1 from "../OrdenMenu/Ensaladas";
+import Sandwiches1 from "../../Components/OrdenMenu/Sandwiches";
+import Cocteles1 from "../../Components/OrdenMenu/Cocteles";
+import Postres1 from "../../Components/OrdenMenu/Postres";
+import Bebidas1 from "../OrdenMenu/Bebidas";
 
 import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,   
+  TableContainer,
   TableHead,
   TableRow,
-}                   
+} from "@material-ui/core";
 
-from "@material-ui/core";
-
-const data = [
- 
-];
-
-
+const data = [];
 
 function OrdenMenu() {
+  const total = data.forEach((subtotal) => {
+    let suma = subtotal;
+  });
 
-
-
-  const total = data.forEach( subtotal =>{
-    let suma = subtotal
-  })
-  
   const [toggle, setToggle] = useState(false);
- 
+
   function Desayuno() {
     setToggle(!toggle);
-       console.log(toggle);
+    console.log(toggle);
   }
   const [toggleEntradas, setToggleEntradas] = useState(false);
 
   function Entradas() {
     setToggleEntradas(!toggleEntradas);
-       console.log(toggleEntradas);
+    console.log(toggleEntradas);
   }
 
   const [toggleRes, setToggleRes] = useState(false);
 
   function Res() {
     setToggleRes(!toggleRes);
-       console.log(toggleRes);
+    console.log(toggleRes);
   }
-  
-  // function Res() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Pescado() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Pollo() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Ensaladas() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Sandwiches() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Cocteles() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Postres() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  // function Bebidas() {
-  //   setToggle(!toggle);
-  //   console.log(toggle);
-  // }
-  
 
-const useStyle = makeStyles({
-  containerTable: {
-    minWidth: 700,                
-    margin: '50px 50px 100px',    
-  },
-  cellTitle:{
-    fontSize: '23px',
-    color: 'bisque',
-    fontFamily: 'roboto slab',
-  },
-  celda:{
-    fontSize: '20px',
-    color: 'beige',
-    paddingLeft: '50px',
-    fontFamily: 'roboto slab',
-},
-});
-const tablaProductos = {
-  contenido: {
-    producto: "",
-    precio:"",
-    eliminar:"",
-    certify: false
-  },
-  checked:{
-    yes: false,
-    no: false
-  }   
-}
-
-  const classes = useStyle()
-
-  const handlesubmit = async (e) => { 
-    e.preventDefault()
-    const objetoData = {}
-  data.forEach(elemento => {
-      Object.assign(
-          objetoData, 
-          {[elemento.producto]:{precio: elemento.precio, elinimar: elemento.eliminar}}
-          )
-  })
+  const [togglePescado, setTogglePescado] = useState(false);
+  function Pescado() {
+    setTogglePescado(!togglePescado);
+    console.log(togglePescado);
   }
+
+  const [togglePollo, setTogglePollo] = useState(false);
+  function Pollo() {
+    setTogglePollo(!togglePollo);
+    console.log(togglePollo);
+  }
+
+  const [toggleEnsaladas, setToggleEnsaladas] = useState(false);
+  function Ensaladas() {
+    setToggleEnsaladas(!toggleEnsaladas);
+    console.log(toggleEnsaladas);
+  }
+
+  const [toggleSandwiches, setToggleSandwiches] = useState(false);
+  function Sandwiches() {
+    setToggleSandwiches(!toggleSandwiches);
+    console.log(toggleSandwiches);
+  }
+
+  const [toggleCocteles, setToggleCocteles] = useState(false);
+  function Cocteles() {
+    setToggleCocteles(!toggleCocteles);
+    console.log(toggleCocteles);
+  }
+
+  const [togglePostres, setTogglePostres] = useState(false);
+  function Postres() {
+    setTogglePostres(!togglePostres);
+    console.log(togglePostres);
+  }
+
+  const [toggleBebidas, setToggleBebidas] = useState(false);
+  function Bebidas() {
+    setToggleBebidas(!toggleBebidas);
+    console.log(toggleBebidas);
+  }
+
+  const useStyle = makeStyles({
+    containerTable: {
+      minWidth: 700,
+      margin: "50px 50px 100px",
+    },
+    cellTitle: {
+      fontSize: "23px",
+      color: "bisque",
+      fontFamily: "roboto slab",
+    },
+    celda: {
+      fontSize: "20px",
+      color: "beige",
+      paddingLeft: "50px",
+      fontFamily: "roboto slab",
+    },
+  });
+  const tablaProductos = {
+    contenido: {
+      producto: "",
+      precio: "",
+      eliminar: "",
+      certify: false,
+    },
+    checked: {
+      yes: false,
+      no: false,
+    },
+  };
+
+  const classes = useStyle();
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    const objetoData = {};
+    data.forEach((elemento) => {
+      Object.assign(objetoData, {
+        [elemento.producto]: {
+          precio: elemento.precio,
+          elinimar: elemento.eliminar,
+        },
+      });
+    });
+  };
 
   return (
     <div className="tabla">
       <h1> Pepito Perez </h1>
-      <div className={classes.containerTable}>   
+      <div className={classes.containerTable}>
         <TableContainer>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell className={classes.cellTitle}> Cantidad por mesa </TableCell>
+                <TableCell className={classes.cellTitle}>
+                  {" "}
+                  Cantidad por mesa{" "}
+                </TableCell>
                 <TableCell className={classes.cellTitle}> Producto </TableCell>
-                <TableCell className={classes.cellTitle}> Precio por unidad</TableCell>
+                <TableCell className={classes.cellTitle}>
+                  {" "}
+                  Precio por unidad
+                </TableCell>
                 <TableCell className={classes.cellTitle}> Subtotal </TableCell>
               </TableRow>
               <TableRow>
@@ -156,106 +162,115 @@ const tablaProductos = {
             </TableHead>
 
             <TableBody>
-              {data.map(celda => (
-              <>
-                <TableRow onSubmit={celda}>
-                  <TableCell className={classes.celda}> {celda.cantidad_por_mesa}</TableCell>
-                  <TableCell className={classes.celda}> {celda.producto} </TableCell>
-                  <TableCell className={classes.celda}> {celda.precio_por_unidad} </TableCell>
-                  <TableCell className={classes.celda}> {celda.subtotal} </TableCell>
+              {data.map((celda) => (
+                <>
+                  <TableRow onSubmit={celda}>
+                    <TableCell className={classes.celda}>
+                      {" "}
+                      {celda.cantidad_por_mesa}
+                    </TableCell>
+                    <TableCell className={classes.celda}>
+                      {" "}
+                      {celda.producto}{" "}
+                    </TableCell>
+                    <TableCell className={classes.celda}>
+                      {" "}
+                      {celda.precio_por_unidad}{" "}
+                    </TableCell>
+                    <TableCell className={classes.celda}>
+                      {" "}
+                      {celda.subtotal}{" "}
+                    </TableCell>
                   </TableRow>
-                <TableRow>
-                  <TableCell className={classes.celda}> {celda.total} </TableCell>
-                </TableRow>
-              </>
+                  <TableRow>
+                    <TableCell className={classes.celda}>
+                      {" "}
+                      {celda.total}{" "}
+                    </TableCell>
+                  </TableRow>
+                </>
               ))}
-            </TableBody>  
+            </TableBody>
           </Table>
         </TableContainer>
       </div>
 
-      <button className='menus' onClick="menus">
+      <button className="menus" onClick="menus">
         Menú
       </button>
-    
-      <Link className="volver" to="/Ordenes"> 
-        <button>
-           Volver 
-        </button>      
+
+      <Link className="volver" to="/Ordenes">
+        <button>Volver</button>
       </Link>
       <div className="bttn">
         <button className="finalizar " onClick="menus">
           Finalizar
         </button>
       </div>
-                                    
+
       <div className="btnFirst">
-        <button className="desayunos" onClick={Desayuno}>  
+        <button className="desayunos" onClick={Desayuno}>
           Desayunos
         </button>
-         <div className={ toggle ? 'active': 'disable'}>
-           {<Desayunos/>}
-         </div>
-        <button className="entradas" onClick={Entradas}>  
+        <div className={toggle ? "active" : "disable"}>
+          {<Desayunos />}</div>
+        <button className="entradas" onClick={Entradas}>
           Entradas
-        </button> 
-        <div className={ toggleEntradas ? 'active': 'disable'}>
-           {<Entradas1/>}
-         </div>  
+        </button>
+        <div className={toggleEntradas ? "active" : "disable"}>
+          {<Entradas1 />}
+        </div>
         <button className="res" onClick={Res}>
           Res
         </button>
-        <div className={ toggleRes ? 'active': 'disable'} >
-           {<Res1/>}
-         </div>
-        <button className="pescado" onClick="{Pescado}"> 
+        <div className={toggleRes ? "active" : "disable"}>
+          {<Res1 />}</div>
+        <button className="pescado" onClick={Pescado}>
           Pescado
         </button>
-        <div className="ContainerPescado" id='fish' >
-           {<Pescado/>}
-         </div>
+        <div className={togglePescado ? "active" : "disable"}>
+          {<Pescado1 />}
+        </div>
       </div>
       <div className="btnSecond">
-        <button className="pollo" onClick="{Pollo}"> 
+        <button className="pollo" onClick={Pollo}>
           Pollo
         </button>
-        <div className="ContainerPollo" id='chicken' >
-           {<Pollo/>}
-         </div>
-        <button className="ensaladas" onClick="{Ensaladas}">
+        <div className={togglePollo ? "active" : "disable"}>{<Pollo1 />}</div>
+        <button className="ensaladas" onClick={Ensaladas}>
           Ensaladas
         </button>
-        <div className="ContainerEnsaladas" id='salad' >
-           {<Ensaladas/>}
-         </div>
-        <button className="sandwiches" onClick="{Sandwiches}">
+        <div className={toggleEnsaladas ? "active" : "disable"}>
+          {<Ensaladas1 />}
+        </div>
+        <button className="sandwiches" onClick={Sandwiches}>
           Sandwiches
         </button>
-        <div className="ContainerSandwiches" id='Sandwich'>
-           {<Sandwiches/>}
-         </div>
+        <div className={toggleSandwiches ? "active" : "disable"}>
+          {<Sandwiches1 />}
+        </div>
       </div>
       <div className="btnThird">
-        <button className="coctel" onClick="{Cocteles}">
+        <button className="coctel" onClick={Cocteles}>
           Cócteles
         </button>
-        <div className="ContainerCocteles" id='cocktails'>
-           {<Cocteles/>}
-         </div>
-        <button className="postres" onClick="{Postres}">
+        <div className={toggleCocteles ? "active" : "disable"}>
+          {<Cocteles1 />}
+        </div>
+        <button className="postres" onClick={Postres}>
           Postres
         </button>
-        <div className="ContainerPostres" id='desserts'>
-           {<Postres/>}
-         </div>
+        <div className={togglePostres ? "active" : "disable"}>
+          {<Postres1 />}
+        </div>
       </div>
       <div className="btnLast">
-        <button className="bebidas" onClick="{Bebidas}">
+        <button className="bebidas" onClick={Bebidas}>
           Bebidas
         </button>
-        <div className="ContainerBebidas" id='drinks' >
-           {<Bebidas/>}
-         </div>
+        <div className={toggleBebidas ? "active" : "disable"}>
+          {<Bebidas1 />}
+        </div>
       </div>
     </div>
   );
